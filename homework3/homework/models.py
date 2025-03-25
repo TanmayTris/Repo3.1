e6ae7c6d9d9d2972cc59c16a9771500367272fe3
TanmayTris <tanmay345@gmail.com>
2025-03-24 19:30:57 -0500

    Update models.py

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -47,7 +47,7 @@ class Classifier(nn.Module):
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
 
-        # Fully connected layers
+        # Fully connected layer
         self.fc1 = nn.Linear(128 * 8 * 8, 512)
         self.fc2 = nn.Linear(512, num_classes)
 
